9182bc36e35c293509ac68a49dbe0691e853af1e
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-03-08 12:57:35 -0700

    Completed exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace SelectionStatementExercise
+﻿using System;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
+        
         {
-            Console.WriteLine("Hello, World!");
+            //Initializing and defining variables
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Guess my Favorite Number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            //Input Section
+
+            
+            var userGuess = userInput;
+
+                //positive statement, userGuess matches "yes"
+                if (userInput == favNumber)
+            {
+                Console.WriteLine("You are one in a thousand!");
+
+            }
+             
+                //failure low
+                else if (userInput < favNumber) 
+            {
+                Console.WriteLine("Wrong! Try again, you guessed too low");     
+            }
+
+                //failure high
+                else if (userInput > favNumber)
+            {
+                Console.WriteLine("Wrong! Try again, you guessed too high");
+
+            }
+
+
+
         }
     }
 }
