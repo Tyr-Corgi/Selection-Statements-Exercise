04df13b7616b06967e21c333482d23c09534fed1
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-05-05 12:09:51 +1200

    initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -5,41 +5,45 @@ namespace SelectionStatementExercise
     public class Program
     {
         static void Main(string[] args)
-        
         {
-            //Initializing and defining variables
+            // Initializing and defining variables
             var r = new Random();
             var favNumber = r.Next(1, 1000);
-            Console.WriteLine("Guess my Favorite Number");
-            var userInput = int.Parse(Console.ReadLine());
+            int userInput;
+            bool guessedCorrectly = false;
 
-            //Input Section
+            // Loop until the user correctly guesses the number
+            while (!guessedCorrectly)
+            {
+                // Prompt the user to guess the number
+                Console.WriteLine("Guess my Favorite Number");
 
-            
-            var userGuess = userInput;
+                // Read the user's input and handle invalid input
+                if (!int.TryParse(Console.ReadLine(), out userInput))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                    continue;
+                }
 
-                //positive statement, userGuess matches "yes"
+                // Compare the user's guess to the target number and provide feedback
                 if (userInput == favNumber)
-            {
-                Console.WriteLine("You are one in a thousand!");
-
-            }
-             
-                //failure low
-                else if (userInput < favNumber) 
-            {
-                Console.WriteLine("Wrong! Try again, you guessed too low");     
-            }
-
-                //failure high
+                {
+                    Console.WriteLine("You are one in a thousand!");
+                    guessedCorrectly = true;
+                }
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine("Wrong! Try again, you guessed too low");
+                }
                 else if (userInput > favNumber)
-            {
-                Console.WriteLine("Wrong! Try again, you guessed too high");
-
+                {
+                    Console.WriteLine("Wrong! Try again, you guessed too high");
+                }
             }
 
-
-
+            // Wait for user input before closing the program
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
